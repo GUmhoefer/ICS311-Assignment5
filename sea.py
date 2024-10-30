85c405adaae67be73c8525d048eccd87e52d068f
gregoru@hawaii.edu <gregoru@hawaii.edu>
2024-10-30 13:46:05 -1000

    Update sea.py

--- sea.py
+++ sea.py
@@ -1,7 +1,6 @@
 class Sea:
     def __init__(self):
         self.islands = {}
-        self.routes = {}
         self.adj = {}
 
     class Island:
@@ -11,18 +10,10 @@ class Sea:
             self.chief_skill = chief_skill
             self.rare_shell = rare_shell
 
-    class Route:
-        def __init__(self, origin, dest, time):
-            self.origin = origin
-            self.dest = dest
-            self.time = time
-
     def add_island(self, name, pop = 500, chief_skill = False, rare_shell = 0):
         self.islands[name] = self.Island(name, pop, chief_skill, rare_shell)
         self.adj[name] = []
 
     def add_route(self, origin, dest, time):
-        if origin not in self.routes:
-            self.routes[origin] = []
-        self.routes[origin].append(self.Route(origin, dest, time))
+        self.adj[origin].append(((dest, time)))
 
