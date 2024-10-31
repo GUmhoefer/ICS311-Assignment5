6ebcd0bb0f73dadd2bbe4df099a582c64bfcd0f9
gregoru@hawaii.edu <gregoru@hawaii.edu>
2024-10-30 14:54:40 -1000

    Update sea.py

--- sea.py
+++ sea.py
@@ -4,7 +4,7 @@ class Sea:
         self.adj = {}
 
     class Island:
-        def __init__(self, name, pop = 500, chief_skill = False, rare_shell = 0)
+        def __init__(self, name, pop = 500, chief_skill = False, rare_shell = 0):
             self.name = name
             self.pop = pop
             self.chief_skill = chief_skill
@@ -19,3 +19,7 @@ class Sea:
             self.add_island(origin)
         self.adj[origin].append(((dest, time)))
 
+    def print_routes(self):
+        for origin in self.adj:
+            for dest, time in self.adj[origin]:
+                print(f"From {origin} to {dest} takes {time} hours")
