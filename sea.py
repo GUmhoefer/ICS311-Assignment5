0d9bce6285e4d6eb5cf19bda3edbe35efcb3820c
jswlee <jswleetaiwan@gmail.com>
2024-10-31 20:13:31 -1000

    First round of edits
    Added mock data and some tests for Sea class initialization and shortest route
    
    Changed chief_skill to last_visit
    
    Added "resource" attribute to Island class
    
    Implemented Dijkstra's algorithm

--- sea.py
+++ sea.py
@@ -1,23 +1,30 @@
+from heapq import heapify, heappop, heappush
 class Sea:
-    def __init__(self):
-        self.islands = {}
-        self.adj = {}
+    def __init__(self, islands, routes):
+        self.islands = {name: self.Island(name, **attributes) for name, attributes in islands.items()}
+        self.adj = {name: {} for name in islands}
+        
+        for origin, dest, time in routes:
+            if origin in self.adj and dest in self.adj:
+                self.adj[origin][dest] = time
 
     class Island:
-        def __init__(self, name, pop = 500, chief_skill = False, rare_shell = 0):
+        def __init__(self, name, pop = 500, last_visit = None, rare_shell=0, resources=None):
             self.name = name
             self.pop = pop
-            self.chief_skill = chief_skill
+            self.last_visit = last_visit
             self.rare_shell = rare_shell
+            self.resources = resources if resources else {}
 
         def __str__(self):
             return (f"\nIsland: {self.name}\n"
                     f"Population: {self.pop}\n"
-                    f"Chief's Skill: {self.chief_skill}\n"
-                    f"Rare Shell: {self.rare_shell}\n\n")
+                    f"Last visit: {self.last_visit}\n"
+                    f"Rare shell: {self.rare_shell}\n"
+                    f"Resources: {self.resources}\n\n")
 
-    def add_island(self, name, pop = 500, chief_skill = False, rare_shell = 0):
-        self.islands[name] = self.Island(name, pop, chief_skill, rare_shell)
+    def add_island(self, name, pop = 500, last_visit = None, rare_shell=0, resources=None):
+        self.islands[name] = self.Island(name, pop, last_visit, rare_shell)
         self.adj[name] = {}
 
     def add_route(self, origin, dest, time):
@@ -34,14 +41,25 @@ class Sea:
     def shortest_paths(self, origin):
         if origin not in self.adj:
             return f"{origin} is not an island in the routes"
-        else:
-            shortest = {}
-            shortest[origin] = 0
-            paths = {island: [] for island in self.adj.keys() if island != origin}
-        print(paths)
-        print(shortest)
+        
+        distances = {island: float('inf') for island in self.islands}
+        distances[origin] = 0
+        priority_queue = [(0, origin)]
 
-        # Still working on this, will eventually be Dijkstra's
+        while priority_queue:
+            current_distance, current_island = heappop(priority_queue)
+
+            if current_distance > distances[current_island]:
+                continue
+
+            for neighbor, time in self.adj[current_island].items():
+                distance = current_distance + time
+
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    heappush(priority_queue, (distance, neighbor))
+
+        return distances
 
  
     def find_route(self, origin, dest):
@@ -68,8 +86,8 @@ class Sea:
         print(self.islands[name])
     
     def print_all_islands(self):
-        for name in self.islands:
-            print(self.islands[name])
+        for name, island in self.islands.items():
+            print(island)
 
     def print_island_names(self):
         for n in self.islands.keys():
@@ -78,6 +96,3 @@ class Sea:
     def print_neighbours(self, name):
         for dest, _ in self.adj[name].items():
             print(f"{name} has a route to {dest}")
-
-        
-    
